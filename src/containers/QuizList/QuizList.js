4ac7198dc7fee6ef6d8eb0d29bdf7e36d198802c
KLIJIN <mdu091008@yandex.ru>
2020-12-21 01:06:03 +0700

    quizlist: стейт перемещен в редакс, добавлен dispatch

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,19 +1,16 @@
 import React, {Component} from "react";
 import s from "./QuizList.module.css"
 import {NavLink} from "react-router-dom"
-import axios from "../../axios/axios";
+//import axios from "../../axios/axios";
 import Loader from "../../components/UI/Loader/Loader"
+import {connect} from "react-redux"
+import fetchQuizes from "../../store/actions/quiz"
 
 
 class QuizList extends Component {
 
-    state = {
-        quizes: [],
-        loader: true
-    }
-
-    renderQuizes() {
-       return this.state.quizes.map((quiz) => {
+  renderQuizes() {  //функция рендерит список тестов.
+       return this.props.quizes.map((quiz) => {
           return (
             <li
               key={quiz.id}
@@ -27,36 +24,37 @@ class QuizList extends Component {
       }
 
 
-    async componentDidMount() {
-        try{
-            const response =  await axios.get("quizes.json")
-            console.log(response)
+     componentDidMount() {
+       this.props.fetchQuizes()
+        // try{
+        //     const response =  await axios.get("quizes.json")
+        //     console.log(response)
 
 
-            const quizesCopy = []
-            Object.keys(response.data).forEach( (key, index) => {
-                quizesCopy.push( {
-                    id: key,
-                    name: `Тест №${index +1}`
-                })
-            } )
+        //     const quizesCopy = []
+        //     Object.keys(response.data).forEach( (key, index) => {
+        //         quizesCopy.push( {
+        //             id: key,
+        //             name: `Тест №${index +1}`
+        //         })
+        //     } )
 
-            this.setState({
-                quizes: quizesCopy,
-                loader: false
-            })
-        } catch (e){
-            console.log(e)
-        }
+        //     this.setState({
+        //         quizes: quizesCopy,
+        //         loader: false
+        //     })
+        // } catch (e){
+        //     console.log(e)
+        // }
       }
 
     render () {
-      
+      console.log("QuizList__render", this.props)
         return(
             <div className={s.QuizList}>
                 <h1> Список тестов </h1>
                 {
-                    this.state.loader
+                    this.props.loader && this.props.quizes !== 0
                     ? <Loader/>
                     :   <ul>
                            {this.renderQuizes()}  {/* функция с круглыми скобками т.к. при рендере она должна автоматически стартовать и отрисовывать список ссылок*/}
@@ -68,4 +66,18 @@ class QuizList extends Component {
     }
 }
 
-export default QuizList;
+
+function mapStateToProps(state) {
+  console.log("QuizList__mapStateToProps", state)
+  return {
+    quizes: state.quiz.quizes,
+    loader: state.quiz.loading
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    fetchQuizes: () => dispatch(fetchQuizes())
+  }
+}
+export default connect (mapStateToProps, mapDispatchToProps) (QuizList);
